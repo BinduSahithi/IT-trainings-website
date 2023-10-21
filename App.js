c06cb73c948a29b54a5e068c5e64d365152f444b
BinduSahithi <108623965+BinduSahithi@users.noreply.github.com>
2023-10-21 19:50:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './components/style.css'
+
+import './App.css';
+import CustPost from './components/CustPost';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+ <CustPost/>
+    
+    </div>
+  );
+};
+export default App;
